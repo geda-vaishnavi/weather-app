45cb3c590c8e2993cff45d6bddd10897f6dce610
Vaishnavi Geda <gedavaishnavi9@gmail.com>
2026-03-23 13:26:53 +0530

    Initial commit: Weather App v1.0

---
+++ script.js
@@ -0,0 +1,36 @@
+const apiKey = "e74b224cd82147ff8cd134242261703";
+
+async function getWeather() {
+  const location = document.getElementById("locationInput").value;
+
+  if (!location) {
+    alert("Please enter a location");
+    return;
+  }
+
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+      alert("Location not found");
+      return;
+    }
+
+    document.getElementById("city").innerText = 
+      `${data.location.name}, ${data.location.country}`;
+
+    document.getElementById("temp").innerText = 
+      `🌡 Temperature: ${data.current.temp_c}°C`;
+
+    document.getElementById("condition").innerText = 
+      `☁️ Condition: ${data.current.condition.text}`;
+
+  } catch (error) {
+    console.error(error);
+    alert("Something went wrong");
+  }
+  
+}
